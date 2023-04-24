3ec8daf86258ccc0123c50a75044071be21e8dad
LuigiGutierrez09 <luigiguti_2000@hotmail.com>
2023-04-24 13:49:24 -0500

    xd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
+import { Diccionario1 } from "./Diccionario.js"
 const gree = name => 'hello '+ name;
 function jiji (){
 
     console.log("XDDD")
+}
+function xd (opc){
+
+switch(opc) {
+    case 1:
+      return Diccionario1.CI_1;
+      break;
+    case 2:
+      // code block
+      break;
+    default:
+      // code block
+  }
 }
